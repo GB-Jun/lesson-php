708e1e3e7193e4ef8aeda06a905ff826a2402bb2
GBJun <windgb369@hotmail.com>
2022-05-24 13:58:42 +0800

    to avoid SQL injection by prepare()

---
+++ fake-data-02.php
@@ -0,0 +1,22 @@
+<?php require __DIR__ . '/parts/connect_db.php';
+
+// 新增資料時, 如果資料從外面來的, 一律用prepare, 以避免SQL注入
+$sql = "INSERT INTO `address_book`(
+    `name`, `email`, `mobile`, 
+    `birthday`, `address`, `created_at`
+    ) VALUES (
+        ?, ?, ?,
+        ?, ?, NOW()
+    )";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute([
+    "李小明's pen",
+    'ming@test.com',
+    '0918123456',
+    '1987-11-23',
+    '南投市',
+]);
+
+
+echo $stmt->rowCount();
